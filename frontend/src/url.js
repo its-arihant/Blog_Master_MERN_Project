413e5886a6bf13b3e100c5035e3ccdb43de80a07
18arihant <arihant18082003@gmail.com>
2025-01-16 22:58:36 +0530

    update

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,2 +1,2 @@
 export const URL="https://blog-master-mern-project-api.onrender.com"
-export const IF="http://localhost:5000/images/"
+export const IF="https://blog-master-mern-project-api.onrender.com/images/"
