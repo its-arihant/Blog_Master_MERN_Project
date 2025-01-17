eedbdfdca0bb7266b5a6ff536fbc7159080c000c
18arihant <arihant18082003@gmail.com>
2025-01-17 11:10:57 +0530

    update

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -70,6 +70,72 @@
 
 // export default Home
 
+// import axios from "axios";
+// import Footer from "../components/Footer";
+// import HomePosts from "../components/HomePosts";
+// import Navbar from "../components/Navbar";
+// import { IF, URL } from "../url";
+// import { useContext, useEffect, useState } from "react";
+// import { Link, useLocation } from "react-router-dom";
+// import Loader from '../components/Loader';
+// import { UserContext } from "../context/UserContext";
+
+// const Home = () => {
+//   const { search } = useLocation();
+//   const [posts, setPosts] = useState([]);
+//   const [noResults, setNoResults] = useState(false);
+//   const [loader, setLoader] = useState(false);
+//   const { user } = useContext(UserContext);
+
+//   const fetchPosts = async () => {
+//     setLoader(true);
+//     try {
+//       const res = await axios.get(`${URL}/api/posts/${search}`);
+//       setPosts(res.data);
+//       setNoResults(res.data.length === 0);
+//       setLoader(false);
+//     } catch (err) {
+//       console.log(err);
+//       setLoader(false);
+//     }
+//   };
+
+//   useEffect(() => {
+//     fetchPosts();
+//   }, [search]);
+
+//   return (
+//     <>
+//       <Navbar />
+//       <div className="px-4 md:px-16 lg:px-32 min-h-[80vh]">
+//         {loader ? (
+//           <div className="h-[40vh] flex justify-center items-center">
+//             <Loader />
+//           </div>
+//         ) : !noResults ? (
+//           posts.map((post) => (
+//             <Link
+//               to={user ? `/posts/post/${post._id}` : "/login"}
+//               key={post._id}
+//               className="block mb-6 hover:scale-105 transition-transform"
+//             >
+//               <HomePosts post={post} />
+//             </Link>
+//           ))
+//         ) : (
+//           <h3 className="text-center font-bold mt-16 text-gray-500">
+//             No posts available
+//           </h3>
+//         )}
+//       </div>
+//       <Footer />
+//     </>
+//   );
+// };
+
+// export default Home;
+
+
 import axios from "axios";
 import Footer from "../components/Footer";
 import HomePosts from "../components/HomePosts";
@@ -95,7 +161,7 @@ const Home = () => {
       setNoResults(res.data.length === 0);
       setLoader(false);
     } catch (err) {
-      console.log(err);
+      console.error("Error fetching posts:", err);
       setLoader(false);
     }
   };
@@ -107,7 +173,7 @@ const Home = () => {
   return (
     <>
       <Navbar />
-      <div className="px-4 md:px-16 lg:px-32 min-h-[80vh]">
+      <div className="container mx-auto px-4 sm:px-8 md:px-16 lg:px-24 min-h-[80vh]">
         {loader ? (
           <div className="h-[40vh] flex justify-center items-center">
             <Loader />
@@ -117,13 +183,13 @@ const Home = () => {
             <Link
               to={user ? `/posts/post/${post._id}` : "/login"}
               key={post._id}
-              className="block mb-6 hover:scale-105 transition-transform"
+              className="block mb-6 hover:scale-105 transition-transform duration-300"
             >
               <HomePosts post={post} />
             </Link>
           ))
         ) : (
-          <h3 className="text-center font-bold mt-16 text-gray-500">
+          <h3 className="text-center font-semibold mt-16 text-gray-600">
             No posts available
           </h3>
         )}
